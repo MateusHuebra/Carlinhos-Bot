fe618627dd30083a2124b76b9228b220730a52c5
mateushuebra <mateushuebra@gmail.com>
2021-12-27 16:45:49 -0300

    fixes to make it work on server

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -10,6 +10,7 @@ class BotController extends Controller
     
     public function listen(Request $request) {
 
+        file_put_contents('php://stderr', json_encode($request->all()));
         $update = (new Factory)->create($request->all());
         $update->handle($request->all());
 
