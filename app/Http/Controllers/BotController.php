3d34e072857f8fb82d2e7815b55d95ba81fec8ae
mateushuebra <mateushuebra@gmail.com>
2021-12-27 16:56:43 -0300

    added pretty json log

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -10,7 +10,7 @@ class BotController extends Controller
     
     public function listen(Request $request) {
 
-        file_put_contents('php://stderr', json_encode($request->all()));
+        file_put_contents('php://stderr', json_encode($request->all(), JSON_PRETTY_PRINT));
         $update = (new Factory)->create($request->all());
         $update->handle($request->all());
 
