fd502179a0df06134f8ab2b5065200ebcfbb4e98
mateushuebra <mateushuebra@gmail.com>
2022-02-17 14:07:57 -0300

    fixed chat entering and leaving

--- app/Services/Updates/LeftChatParticipant.php
+++ app/Services/Updates/LeftChatParticipant.php
@@ -9,9 +9,10 @@ class LeftChatParticipant implements Update {
 
     function handle(array $update) {
         if($update['message']['left_chat_member']['id']==env('T_BOT_ID')) {
-            return Pattern::firstWhere('name', 'welcome carlinhos');
+            //return Pattern::firstWhere('name', 'welcome carlinhos');
             $chat = Chat::firstWhere('id', $update['message']['chat']['id']);
             $chat->delete();
+            return null;
         }
         return Pattern::firstWhere('name', 'goodbye');
     }
--- app/Services/Updates/NewChatParticipant.php
+++ app/Services/Updates/NewChatParticipant.php
@@ -9,11 +9,11 @@ class NewChatParticipant implements Update {
 
     function handle(array $update) {
         if($update['message']['new_chat_member']['id']==env('T_BOT_ID')) {
-            return Pattern::firstWhere('name', 'welcome carlinhos');
             $chat = new Chat();
             $chat->id = $update['message']['chat']['id'];
             $chat->custom_welcome = null;
             $chat->save();
+            return Pattern::firstWhere('name', 'welcome carlinhos');
         }
         return Pattern::firstWhere('name', 'welcome');
     }
