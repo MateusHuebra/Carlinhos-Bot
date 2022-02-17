5795ed6903920d6514c296ae7e5496da72bb9cf0
mateushuebra <mateushuebra@gmail.com>
2022-02-17 14:09:19 -0300

    nsfw is false by default

--- app/Services/Updates/NewChatParticipant.php
+++ app/Services/Updates/NewChatParticipant.php
@@ -11,6 +11,7 @@ class NewChatParticipant implements Update {
         if($update['message']['new_chat_member']['id']==env('T_BOT_ID')) {
             $chat = new Chat();
             $chat->id = $update['message']['chat']['id'];
+            $chat->nsfw = false;
             $chat->custom_welcome = null;
             $chat->save();
             return Pattern::firstWhere('name', 'welcome carlinhos');
